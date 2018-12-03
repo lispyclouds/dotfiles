755b4d294b5b0aa1ba6232f53ec8364a6321ee33
Rahul De <rahul080327@gmail.com>
2018-12-03 15:43:37 +0100

    Add Emacs, lolcat

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins      [[venantius/ultra "LATEST"]
                        [lein-exec "LATEST"]
-                       [lein-cljfmt "LATEST"]]
+                       [lein-cljfmt "LATEST"]
+                       [cider/cider-nrepl "LATEST"]]
         :dependencies [[cljfmt "LATEST"]
                        [mvxcvi/whidbey "LATEST"]]
         :repl-options {:prompt  #(str "\u001B[32m[\u001B[32m" % "\u001B[32m]\u001B[32m(λ)\u001B[m ")
