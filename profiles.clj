22754e1614ac54c4d085fd87e30087b92af0efe1
Rahul De <rahul080327@gmail.com>
2018-12-12 16:50:58 +0100

    clean profiles

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,5 @@
-{:user {:plugins      [[venantius/ultra "LATEST"]
-                       [lein-exec "LATEST"]
-                       [lein-cljfmt "LATEST"]
-                       [cider/cider-nrepl "LATEST"]]
-        :dependencies [[cljfmt "LATEST"]
-                       [mvxcvi/whidbey "LATEST"]]
+{:user {:plugins      [[venantius/ultra "0.5.2"]]
+        :dependencies [[mvxcvi/whidbey "1.3.2"]
+                       [nrepl "0.5.3"]]
         :repl-options {:prompt  #(str "\u001B[32m[\u001B[32m" % "\u001B[32m]\u001B[32m(λ)\u001B[m ")
                        :welcome (println "All hail the λ")}}}
