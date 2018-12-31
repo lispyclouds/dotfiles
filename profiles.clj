27603baf59c201980fc40260cc948ec740b2cac2
Rahul De <rahul080327@gmail.com>
2018-12-31 11:20:43 +0100

    Add js layer, remove nREPL dep

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,2 @@
-{:user {:dependencies [[nrepl "0.5.3"]]
-        :repl-options {:prompt  #(str "\u001B[32m[\u001B[32m" % "\u001B[32m]\u001B[32m(λ)\u001B[m ")
+{:user {:repl-options {:prompt  #(str "\u001B[32m[\u001B[32m" % "\u001B[32m]\u001B[32m(λ)\u001B[m ")
                        :welcome (println "All hail the λ")}}}
