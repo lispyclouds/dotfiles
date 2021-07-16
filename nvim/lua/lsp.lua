e8f49ba042ee28954e9fdfdb6826d84313aa0965
Rahul De <rahul080327@gmail.com>
2021-07-16 21:55:15 +0200

    organise even better

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,4 +1,4 @@
-lspconfig = require("lspconfig")
+local lspconfig = require("lspconfig")
 
 lsps = {
   "clojure_lsp",
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,90 +1,103 @@
 local execute      = vim.api.nvim_command
 local fn           = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local plugins      = {
+  ["folke/tokyonight.nvim"] = {}, -- colorscheme
+
+  ["wbthomason/packer.nvim"] = {},
+
+  ["kyazdani42/nvim-tree.lua"] = {},
+
+  ["kyazdani42/nvim-web-devicons"] = {},
+
+  ["ntpeters/vim-better-whitespace"] = {},
+
+  ["dense-analysis/ale"] = {},
+
+  ["neovim/nvim-lspconfig"] = {},
+
+  ["famiu/bufdelete.nvim"] = {},
+
+  ["nvim-treesitter/nvim-treesitter"] = {
+    run = ":TSUpdate",
+  },
+
+  ["nvim-telescope/telescope.nvim"] = {
+    requires = {
+      {"nvim-lua/popup.nvim"},
+      {"nvim-lua/plenary.nvim"},
+    },
+  },
+
+  ["akinsho/nvim-bufferline.lua"] = {
+    requires = "kyazdani42/nvim-web-devicons",
+    config   = function()
+      require("bufferline").setup{}
+    end,
+  },
+
+  ["hashivim/vim-terraform"] = {
+    ft = {"terraform"},
+  },
+
+  ["hrsh7th/nvim-compe"] = {
+    config = function()
+      require("compe").setup {
+        enabled      = true,
+        autocomplete = true,
+        source       = {
+          path     = true,
+          buffer   = true,
+          calc     = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+        },
+      }
+    end,
+  },
+
+  ["windwp/windline.nvim"] = {
+    config = function()
+      require("wlsample.bubble")
+    end,
+  },
+
+  ["lewis6991/gitsigns.nvim"] = {
+    requires = {"nvim-lua/plenary.nvim"},
+    config   = function()
+      require("gitsigns").setup()
+    end,
+  },
+
+  ["Olical/conjure"] = {
+    ft = {"clojure"},
+  },
+
+  ["guns/vim-sexp"] = {
+    ft = {"clojure"},
+  },
+
+  ["eraserhd/parinfer-rust"] = {
+    ft  = {"clojure"},
+    run = {"cargo build --release"},
+  },
+}
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
   execute "packadd packer.nvim"
 end
 
-return require("packer").startup(
-  function()
-    -- Packer can manage itself
-    use "wbthomason/packer.nvim"
-
-    -- Colorscheme
-    use "folke/tokyonight.nvim"
-
-    use "kyazdani42/nvim-tree.lua"
-    use "kyazdani42/nvim-web-devicons"
+require("packer").startup(
+function()
+  for plugin, conf in pairs(plugins) do
     use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
+      plugin,
+      requires = conf.requires,
+      run      = conf.run,
+      config   = conf.config,
+      ft       = conf.ft,
     }
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}},
-    }
-    use {
-      "akinsho/nvim-bufferline.lua",
-      requires = "kyazdani42/nvim-web-devicons",
-      config   = function()
-        require("bufferline").setup{}
-      end,
-    }
-    use {
-      "rust-lang/rust.vim",
-      ft = {"rust"},
-    }
-    use "ntpeters/vim-better-whitespace"
-    use {
-      "hashivim/vim-terraform",
-      ft = {"terraform"},
-    }
-    use "dense-analysis/ale"
-    use "neovim/nvim-lspconfig"
-    use {
-      "hrsh7th/nvim-compe",
-      config = function()
-        require("compe").setup {
-          enabled      = true,
-          autocomplete = true,
-
-          source = {
-            path     = true,
-            buffer   = true,
-            calc     = true,
-            nvim_lsp = true,
-            nvim_lua = true,
-          },
-        }
-      end,
-    }
-    use {
-      "windwp/windline.nvim",
-      config = function()
-        require("wlsample.bubble")
-      end,
-    }
-    use {
-      "lewis6991/gitsigns.nvim",
-      requires = {"nvim-lua/plenary.nvim"},
-      config   = function()
-        require("gitsigns").setup()
-      end,
-    }
-    use {
-      "Olical/conjure",
-      ft = {"clojure"},
-    }
-    use {
-      "guns/vim-sexp",
-      ft = {"clojure"},
-    }
-    use {
-      "eraserhd/parinfer-rust",
-      run = {"cargo build --release"},
-    }
-    use "famiu/bufdelete.nvim"
   end
+end
 )
