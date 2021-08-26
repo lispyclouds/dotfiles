b03202adb66da1b8f6045917dfe6832921406bdd
Rahul De <rahul080327@gmail.com>
2021-08-26 08:53:03 +0200

    colorscheme conf

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -6,9 +6,12 @@ local catppuccino = require("catppuccino")
 catppuccino.setup({
   integrations = {
     gitsigns   = true,
-		telescope  = true,
-		nvimtree   = true,
-		which_key  = true,
+    telescope  = true,
+    nvimtree   = {
+      enabled   = true,
+      show_root = true,
+    },
+    which_key  = true,
     bufferline = true,
     markdown   = true,
   },
