d2c72a6c9541f67e006d6b6855a1eb2858b35b38
Rahul De <rahul080327@gmail.com>
2022-04-08 21:08:07 +0100

    fonts

--- nvim/lua/ux.lua
+++ nvim/lua/ux.lua
@@ -34,7 +34,7 @@ vim.o.mouse = "a"
 vim.o.termguicolors = true
 
 if vim.loop.os_uname().sysname == "Linux" then
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
 else
   vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
 end
