6d64114e8120abd2706df0516698f00730368d86
Rahul De <rahul080327@gmail.com>
2022-06-26 22:48:29 +0100

    Better visual colour

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -4,6 +4,9 @@ vim.o.termguicolors = true
 require("kanagawa").setup({
   dimInactive = true,
   globalStatus = true,
+  colors = {
+    waveBlue1 = "#49473E", -- Visual selection colour: https://github.com/rebelot/kanagawa.nvim#color-palette
+  },
 })
 
 vim.cmd("colorscheme kanagawa")
