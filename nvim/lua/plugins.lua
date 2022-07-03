07617ed0ab94780c0b62e00b3ee70075359faaee
Rahul De <rahul080327@gmail.com>
2022-07-03 11:12:21 +0100

    telescope prompt

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,11 @@ local plugins = {
     },
     config = function()
       require("telescope").setup({
+        defaults = {
+          layout_config = {
+            prompt_position = "top",
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
