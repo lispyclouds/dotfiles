4b6a375f2a89f95568c843e9355e86b7c0504174
Rahul De <rahul080327@gmail.com>
2022-08-17 08:08:30 +0100

    better cursorline

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,4 +1,10 @@
 vim.o.background = "dark"
 vim.o.termguicolors = true
 
-require("everblush").setup({})
+require("everblush").setup({
+  override = {
+    CursorLineNr = {
+      fg = require("everblush.core").get_colors().foreground,
+    },
+  },
+})
