32745305ab4b3f06e17efd14411ba2641edf2f68
Rahul De <rahul080327@gmail.com>
2022-08-26 07:32:15 +0100

    better wez kanagawa

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -39,14 +39,30 @@ local keymap = {
   },
 }
 
+local kanagawa = {
+  ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+  background = "#1f1f28",
+  brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+  cursor_bg = "#c8c093",
+  cursor_border = "#c8c093",
+  cursor_fg = "#c8c093",
+  foreground = "#dcd7ba",
+  indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+  scrollbar_thumb = "#16161d",
+  selection_bg = "#2d4f67",
+  selection_fg = "#c8c093",
+  split = "#16161d",
+}
+
 local font, font_size = font_conf()
 
 return {
-  color_scheme = "kanagawabones",
+  colors = kanagawa,
   enable_wayland = true,
-  freetype_load_target = "HorizontalLcd",
   font_size = font_size,
   font = wezterm.font(font, { weight = "Bold" }),
+  force_reverse_video_cursor = true,
+  freetype_load_target = "HorizontalLcd",
   hide_tab_bar_if_only_one_tab = true,
   keys = keymap,
   tab_bar_at_bottom = true,
