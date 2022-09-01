7902425372065b05ff0562fa631a4ea4ad98badf
Rahul De <rahul080327@gmail.com>
2022-09-01 08:58:22 +0100

    simpler catppuccin conf

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -12,22 +12,8 @@ require("catppuccin").setup({
     enabled = true,
   },
   integrations = {
-    treesitter = true,
-    treesitter_context = true,
-    native_lsp = {
-      enabled = true,
-    },
-    cmp = true,
-    gitsigns = true,
-    telescope = true,
     neotree = {
       enabled = true,
-      show_root = true,
-      transparent_panel = false,
-    },
-    indent_blankline = {
-      enabled = true,
-      colored_indent_levels = true,
     },
   },
 })
