b3a69582de858c7b7e987263dd223f14f386deac
Rahul De <rahul080327@gmail.com>
2022-10-10 07:35:27 +0530

    Better mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,21 +1,27 @@
+local mappings = {
+  ["gd"] = function() vim.lsp.buf.definition() end,
+  ["H"] = ":BufferLineCyclePrev<CR>",
+  ["L"] = ":BufferLineCycleNext<CR>",
+  ["<leader>bc"] = ":Bwipeout<CR>",
+  ["<leader>ca"] = function() vim.lsp.buf.code_action() end,
+  ["<leader>c"] = '"_c',
+  ["<leader>C"] = '"_C',
+  ["<leader><CR>"] = ":noh<CR>",
+  ["<leader>d"] = '"_d',
+  ["<leader>D"] = '"_D',
+  ["<leader>dd"] = '"_dd',
+  ["<leader>d"] = function() vim.lsp.buf.hover() end,
+  ["<leader>ff"] = function() require("telescope.builtin").find_files() end,
+  ["<leader>f"] = function() require("telescope.builtin").live_grep() end,
+  ["<leader>n"] = ":Neotree toggle<CR>",
+  ["<leader>pm"] = function() vim.o.relativenumber = not vim.o.relativenumber end,
+  ["<leader>r"] = function() vim.lsp.buf.rename() end,
+  ["<leader>x"] = '"_x',
+}
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
-local normal_mappings = {
-  ["<Leader><CR>"] = ":noh<CR>",
-  ["<Leader>n"] = ":Neotree toggle<CR>",
-  ["<Leader>l"] = ":BufferLineCycleNext<CR>",
-  ["<Leader>h"] = ":BufferLineCyclePrev<CR>",
-  ["<Leader>bc"] = ":Bwipeout<CR>",
-  ["<Leader>ff"] = function() require("telescope.builtin").find_files() end,
-  ["<Leader>s"] = function() require("telescope.builtin").live_grep() end,
-  ["<Leader>pm"] = function() vim.o.relativenumber = not vim.o.relativenumber end,
-  ["<Leader>ca"] = function() vim.lsp.buf.code_action() end,
-  ["gd"] = function() vim.lsp.buf.definition() end,
-  ["<Leader>d"] = function() vim.lsp.buf.hover() end,
-  ["<Leader>r"] = function() vim.lsp.buf.rename() end,
-}
-
-for mapping, action in pairs(normal_mappings) do
-  vim.keymap.set("n", mapping, action, { silent = true })
+for mapping, action in pairs(mappings) do
+  vim.keymap.set({ "n", "v" }, mapping, action, { silent = true })
 end
