e3b71a608b3f1df4abe93ecae023f8a0af0164ce
Rahul De <rahul080327@gmail.com>
2022-10-11 13:00:37 +0530

    inverse blackhole mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,22 +1,24 @@
 local mappings = {
+  ["c"] = '"_c', -- blackhole
+  ["C"] = '"_C', -- blackhole
+  ["d"] = '"_d', -- blackhole
+  ["D"] = '"_D', -- blackhole
+  ["dd"] = '"_dd', -- blackhole
   ["gd"] = function() vim.lsp.buf.definition() end,
   ["H"] = ":BufferLineCyclePrev<CR>",
   ["L"] = ":BufferLineCycleNext<CR>",
   ["<leader>bc"] = ":Bwipeout<CR>",
   ["<leader>ca"] = function() vim.lsp.buf.code_action() end,
-  ["<leader>c"] = '"_c',
-  ["<leader>C"] = '"_C',
   ["<leader><CR>"] = ":noh<CR>",
-  ["<leader>d"] = '"_d',
-  ["<leader>D"] = '"_D',
-  ["<leader>dd"] = '"_dd',
-  ["<leader>d"] = function() vim.lsp.buf.hover() end,
+  ["<leader>dd"] = "dd", -- usual
+  ["<leader>d"] = "d", -- usual
+  ["<leader>D"] = "D", -- usual
   ["<leader>ff"] = function() require("telescope.builtin").find_files() end,
   ["<leader>f"] = function() require("telescope.builtin").live_grep() end,
+  ["<leader>h"] = function() vim.lsp.buf.hover() end,
   ["<leader>n"] = ":Neotree toggle<CR>",
   ["<leader>pm"] = function() vim.o.relativenumber = not vim.o.relativenumber end,
   ["<leader>r"] = function() vim.lsp.buf.rename() end,
-  ["<leader>x"] = '"_x',
 }
 
 vim.g.mapleader = " "
