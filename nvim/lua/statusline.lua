49d6c59ea834bca664c8a854daddb0a739205038
Rahul De <rahul080327@gmail.com>
2022-12-28 11:47:59 +0000

    [statusline] bubbles

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,169 +1,64 @@
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
+-- Bubbles config for lualine
+-- Author: lokesh-krishna, MIT Licensed
 
 return {
   setup = function()
     local colors = {
-      bg = "#202328",
-      fg = "#bbc2cf",
-      yellow = "#ecbe7b",
-      cyan = "#008080",
-      darkblue = "#081633",
-      green = "#98be65",
-      orange = "#ff8800",
-      violet = "#a9a1e1",
-      magenta = "#c678dd",
-      blue = "#51afef",
-      red = "#ec5f67",
+      blue = "#80a0ff",
+      cyan = "#79dac8",
+      black = "#080808",
+      white = "#c6c6c6",
+      red = "#ff5189",
+      violet = "#d183e8",
+      grey = "#303030",
     }
 
-    local fn = vim.fn
+    local bubbles_theme = {
+      normal = {
+        a = { fg = colors.black, bg = colors.violet },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.black, bg = colors.black },
+      },
 
-    local conditions = {
-      buffer_not_empty = function() return fn.empty(fn.expand("%:t")) ~= 1 end,
-      hide_in_width = function() return fn.winwidth(0) > 80 end,
-      check_git_workspace = function()
-        local filepath = fn.expand("%:p:h")
-        local gitdir = fn.finddir(".git", filepath .. ";")
-        return gitdir and #gitdir > 0 and #gitdir < #filepath
-      end,
+      insert = { a = { fg = colors.black, bg = colors.blue } },
+      visual = { a = { fg = colors.black, bg = colors.cyan } },
+      replace = { a = { fg = colors.black, bg = colors.red } },
+
+      inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.black, bg = colors.black },
+      },
     }
 
-    local config = {
+    require("lualine").setup({
       options = {
-        component_separators = "",
-        section_separators = "",
-        theme = {
-          normal = { c = { fg = colors.fg, bg = colors.bg } },
-          inactive = { c = { fg = colors.fg, bg = colors.bg } },
-        },
+        theme = bubbles_theme,
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        lualine_c = {},
+        lualine_a = {
+          { "mode", separator = { left = "" }, right_padding = 2 },
+        },
+        lualine_b = { "filename", "branch" },
+        lualine_c = { "fileformat" },
         lualine_x = {},
+        lualine_y = { "diagnostics", "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
       },
       inactive_sections = {
-        lualine_a = {},
+        lualine_a = { "filename" },
         lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
         lualine_c = {},
         lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
       },
-    }
-
-    local function ins_left(component) table.insert(config.sections.lualine_c, component) end
-
-    local function ins_right(component) table.insert(config.sections.lualine_x, component) end
-
-    ins_left({
-      function() return "▊" end,
-      color = { fg = colors.blue },
-      padding = { left = 0, right = 1 },
+      tabline = {},
+      extensions = {},
     })
-
-    ins_left({
-      function() return "" end,
-      color = function()
-        local mode_color = {
-          n = colors.red,
-          i = colors.green,
-          v = colors.blue,
-          [""] = colors.blue,
-          V = colors.blue,
-          c = colors.magenta,
-          no = colors.red,
-          s = colors.orange,
-          S = colors.orange,
-          [""] = colors.orange,
-          ic = colors.yellow,
-          R = colors.violet,
-          Rv = colors.violet,
-          cv = colors.red,
-          ce = colors.red,
-          r = colors.cyan,
-          rm = colors.cyan,
-          ["r?"] = colors.cyan,
-          ["!"] = colors.red,
-          t = colors.red,
-        }
-        return { fg = mode_color[fn.mode()] }
-      end,
-      padding = { right = 1 },
-    })
-
-    ins_left({
-      "filesize",
-      cond = conditions.buffer_not_empty,
-    })
-
-    ins_left({
-      "filename",
-      cond = conditions.buffer_not_empty,
-      color = { fg = colors.magenta, gui = "bold" },
-    })
-
-    ins_left({ "location" })
-
-    ins_left({ "progress", color = { fg = colors.fg, gui = "bold" } })
-
-    ins_left({
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      symbols = { error = " ", warn = " ", info = " " },
-      diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
-      },
-    })
-
-    ins_left({
-      function() return "%=" end,
-    })
-
-    ins_right({
-      "o:encoding",
-      fmt = string.upper,
-      cond = conditions.hide_in_width,
-      color = { fg = colors.green, gui = "bold" },
-    })
-
-    ins_right({
-      "fileformat",
-      fmt = string.upper,
-      icons_enabled = false,
-      color = { fg = colors.green, gui = "bold" },
-    })
-
-    ins_right({
-      "branch",
-      icon = "",
-      color = { fg = colors.violet, gui = "bold" },
-    })
-
-    ins_right({
-      "diff",
-      symbols = { added = " ", modified = "柳 ", removed = " " },
-      diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-      },
-      cond = conditions.hide_in_width,
-    })
-
-    ins_right({
-      function() return "▊" end,
-      color = { fg = colors.blue },
-      padding = { left = 1 },
-    })
-
-    require("lualine").setup(config)
   end,
 }
