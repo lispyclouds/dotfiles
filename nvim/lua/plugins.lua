f03bd012e922d26560847e38c0796f846d4ba7f6
Rahul De <rahul080327@gmail.com>
2023-01-04 22:22:29 +0000

    [colorscheme] tokioooo

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,20 +4,45 @@ local lisps = { "clojure", "fennel" }
 return {
   "kyazdani42/nvim-web-devicons",
   {
-    "sam4llis/nvim-tundra",
+    "folke/tokyonight.nvim",
     config = function()
-      require("nvim-tundra").setup({
-        transparent_background = false,
-        plugins = {
-          cmp = true,
-          gitsigns = true,
-          lsp = true,
-          telescope = true,
-          treesitter = true,
-        },
+      local tokyo = require("tokyonight")
+
+      tokyo.setup({
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
       })
 
-      vim.cmd("colorscheme tundra")
+      tokyo._load("night")
     end,
   },
   {
