89c451537a3e59f4265c79f596923b59b1c6d2e9
Rahul De <rahul080327@gmail.com>
2023-02-12 17:02:13 +0000

    [colours] nvim-tundra

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,45 +4,25 @@ local lisps = { "clojure", "fennel" }
 return {
   "nvim-tree/nvim-web-devicons",
   {
-    "folke/tokyonight.nvim",
+    "sam4llis/nvim-tundra",
     config = function()
-      local tokyo = require("tokyonight")
+      local tundra = require("nvim-tundra")
 
-      tokyo.setup({
-        on_highlights = function(hl, c)
-          local prompt = "#2d3149"
-
-          hl.TelescopeNormal = {
-            bg = c.bg_dark,
-            fg = c.fg_dark,
-          }
-          hl.TelescopeBorder = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.TelescopePromptNormal = {
-            bg = prompt,
-          }
-          hl.TelescopePromptBorder = {
-            bg = prompt,
-            fg = prompt,
-          }
-          hl.TelescopePromptTitle = {
-            bg = prompt,
-            fg = prompt,
-          }
-          hl.TelescopePreviewTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.TelescopeResultsTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-        end,
+      tundra.setup({
+        plugins = {
+          cmp = true,
+          context = false,
+          dbui = false,
+          gitsigns = true,
+          lsp = true,
+          neogit = false,
+          nvimtree = false,
+          telescope = true,
+          treesitter = true,
+        },
       })
 
-      tokyo._load("night")
+      tundra.load()
     end,
   },
   {
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -52,7 +52,37 @@ local window_decoration = "TITLE | RESIZE"
 if is_linux() then window_decoration = "RESIZE" end
 
 return {
-  color_scheme = "tokyonight",
+  colors = {
+    foreground = "#D1D5DB",
+    background = "#111827",
+    cursor_fg = "#111827",
+    cursor_bg = "#D1D5DB",
+    cursor_border = "#111827",
+    selection_fg = "#DDD6FE",
+    selection_bg = "#374151",
+    scrollbar_thumb = "#6B7280",
+    split = "#6B7280",
+    ansi = {
+      "#6B7280",
+      "#FCA5A5",
+      "#B1E3AD",
+      "#FBC19D",
+      "#BAE6FD",
+      "#957FB8",
+      "#DDD6FE",
+      "#A5F3FC",
+    },
+    brights = {
+      "#6B7280",
+      "#FCA5A5",
+      "#B1E3AD",
+      "#FBC19D",
+      "#BAE6FD",
+      "#938AA9",
+      "#DDD6FE",
+      "#A5F3FC",
+    },
+  },
   enable_wayland = true,
   font_size = font_size,
   font = wezterm.font(font, { weight = "Bold" }),
