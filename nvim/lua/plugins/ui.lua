1e1754c9b78e99c72d0bdee72d8c16a76560656d
Rahul De <rahul080327@gmail.com>
2023-11-19 13:35:07 +0000

    [nvim] inline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -32,4 +32,18 @@ return {
     event = "BufRead",
     config = true,
   },
+  {
+    "echasnovski/mini.pick",
+    config = true,
+    lazy = true,
+    opts = {
+      mappings = {
+        move_down = "<C-j>",
+        move_up = "<C-k>",
+      },
+      options = {
+        use_cache = true,
+      },
+    },
+  },
 }
