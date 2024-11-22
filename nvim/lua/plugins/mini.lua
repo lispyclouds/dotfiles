5097ca47524bbe48ffd7d8690b2d4d94a84422d7
Rahul De <rahul080327@gmail.com>
2024-11-22 09:11:50 +0000

    [housekeeping] clean mini

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,27 +5,21 @@ return {
   config = function()
     local next, prev = "<C-j>", "<C-k>"
     local s = vim.keymap.set
-    local none = {}
-    local conf = {
-      ["mini.bufremove"] = none,
-      ["mini.files"] = none,
-      ["mini.icons"] = none,
-      ["mini.trailspace"] = none,
-      ["mini.completion"] = none,
-      ["mini.pick"] = {
-        mappings = {
-          move_down = next,
-          move_up = prev,
-        },
-        options = {
-          use_cache = true,
-        },
-      },
-    }
 
-    for plug, opts in pairs(conf) do
-      require(plug).setup(opts)
-    end
+    require("mini.bufremove").setup()
+    require("mini.files").setup()
+    require("mini.icons").setup()
+    require("mini.trailspace").setup()
+    require("mini.completion").setup()
+    require("mini.pick").setup({
+      mappings = {
+        move_down = next,
+        move_up = prev,
+      },
+      options = {
+        use_cache = true,
+      },
+    })
 
     -- better mini.completion nav
     s("i", next, [[pumvisible() ? "\<C-n>" : "\]] .. next .. [["]], { expr = true })
