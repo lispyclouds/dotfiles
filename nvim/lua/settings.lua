0d8ae5403b8fb2e9f9072448fcfe7c1011874542
Rahul De <rahul080327@gmail.com>
2026-04-11 14:32:08 +0100

    [nvim] spring cleaning

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,35 +1,25 @@
-local undo_dir = vim.loop.os_homedir() .. "/.vim/undodir"
+local undo_dir = vim.uv.os_homedir() .. "/.vim/undodir"
 if not vim.fn.isdirectory(undo_dir) then
   vim.fn.mkdir(undo_dir, "", 0700)
 end
 
-local encoding = "utf-8"
 local opts = {
-  autoindent = true,
   background = "dark",
   backup = false,
   clipboard = "unnamedplus",
-  cmdheight = 1,
   completeopt = "menuone,noselect,fuzzy,popup",
   cursorline = true,
-  encoding = encoding,
-  errorbells = false,
   expandtab = true,
-  fileencoding = encoding,
+  fileencoding = "utf-8",
   fileformats = "unix,dos,mac",
-  hidden = true,
   history = 1000,
-  hlsearch = true,
   ignorecase = true,
   incsearch = true,
   laststatus = 3,
-  magic = true,
-  mat = 2,
   mouse = "nvi",
   number = true,
   pumborder = "rounded",
   relativenumber = true,
-  ruler = true,
   scrolloff = 7,
   shiftwidth = 2,
   shortmess = "Ic",
@@ -37,18 +27,15 @@ local opts = {
   showmode = false,
   signcolumn = "yes:1",
   smartindent = true,
-  smarttab = true,
   swapfile = false,
   tabstop = 2,
   termguicolors = true,
   timeoutlen = 500,
   undodir = undo_dir,
   undofile = true,
-  updatetime = 100,
-  visualbell = false,
+  updatetime = 250,
   winborder = "rounded",
   wrap = true,
-  writebackup = false,
 }
 
 for opt, val in pairs(opts) do
